4f36a19a7ee95d6b86521141c2fe71b82371da98
dubillaa477 <43178679+dubillaa477@users.noreply.github.com>
2018-10-29 19:12:04 -0600

    Add project files.

---
+++ Lab07.Test/PersonTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab07.Test
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void APersonIsCreated()
+        {
+            //Arrange
+            PersonBL logic = new PersonBL();
+
+            //Act
+
+            //Assert
+
+        }
+
+        [TestMethod]
+        public void APersonMustHaveaName()
+        {
+
+        }
+
+        [TestMethod]
+        public void TheAgeOfAPersonIsCalculatedCorrectly()
+        {
+
+        }
+    }
+}
---
+++ Lab07.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab07.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab07.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40097da8-3867-4a9b-a2cf-a63bc760b27c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
